eb3555d8b2cfc9967e8fe8c8a5c25274e1a822a8
dajandro <dajandro95@gmail.com>
2017-03-01 01:25:25 -0600

    WebServer creation

--- src/gamecenter/GameCenter.java
+++ src/gamecenter/GameCenter.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gamecenter;
+package GameCenter;
+
+import WebServer.WebServer;
 
 /**
  *
@@ -14,8 +16,10 @@ public class GameCenter {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         // TODO code application logic here
+        WebServer gamecenter_server = new WebServer();
+        gamecenter_server.run();
     }
     
 }
